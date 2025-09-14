69ad957b8e726993f15887804f188c8d49a0b14a
rlarudqm <rlarudqm110309@gmail.com>
2025-09-14 14:59:21 +0900

    Add files via upload

---
+++ 091402.py
@@ -0,0 +1,8 @@
+friends = ["래현","민호", "헌서"]
+friends.insert(0,"재청")
+print(friends)
+name=friends.pop() #제일 마지막 요소를 뽑아냄
+print("나간사람:", name)
+print(friends)
+friends.remove("래현") #지정된 요소를 제거할수 있다
+print(friends)
